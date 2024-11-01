8cc85a268193a2ed448358f46a4da8a91d41246b
CartierTimes <ronvoslk@gmail.com>
2024-10-31 19:51:35 -0600

    New RPG
    This Branch will be the start of an RPG project. Currently barebones but will add more stuff much later

---
+++ demoRPG/src/MainRPG.java
@@ -0,0 +1,13 @@
+
+
+
+/**
+ * Building a classic RPG game here will classic movements and interactions
+ * Barebones currently but will implement basic combat and etc later
+ */
+public class MainRPG {
+    public static void main(String[] args) {
+
+        
+    }
+}
