28f410b2f541a1897159ac274bbf10aba0c02020
CartierTimes <ronvoslk@gmail.com>
2024-10-31 20:34:07 -0600

    Initial Adds
    Currently started with a JFrame. Made the options of closing the applications with the "x" icon on the top right. Also keeping it to one side. Made the Title for the game as well

--- demoRPG/src/MainRPG.java
+++ demoRPG/src/MainRPG.java
@@ -1,13 +1,24 @@
-
+import javax.swing.JFrame;
 
 
 /**
  * Building a classic RPG game here will classic movements and interactions
- * Barebones currently but will implement basic combat and etc later
+ * Barebones currently but will implement basic combat and others later
  */
 public class MainRPG {
+
+    //Inside the main here will establish the first connection in building this
     public static void main(String[] args) {
 
-        
+        JFrame window = new JFrame();
+
+        //This here will make sure the program properly closes when clicking the x
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //This to keep the game on one size
+        window.setResizable(false);
+
+        window.setTitle("Adventures of Jimmy");
+
     }
 }
